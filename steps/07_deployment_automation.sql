e18a3b5cdb7be02bac95a3898371c35053e189ce
snemerXanterra <114095483+snemerXanterra@users.noreply.github.com>
2025-10-28 15:14:45 +0000

    parametrize pipeline

--- steps/07_deployment_automation.sql
+++ steps/07_deployment_automation.sql
@@ -4,10 +4,23 @@ Commit and push your changes from 06_separate_dev_and_prod_environments.sql to t
 This will start the preconfigured CI/CD pipeline and deploy the changes
 -----------------------------------------------------------------------------------------------
 */
+ALTER USER snemer SET RSA_PUBLIC_KEY='MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsmfzTeCf6LPnfnzU7fvo
+GsOfgXBLXzOb2ePDR5IMAcM2sa9eznzPDdljdXH78IRJuvTzLvVZiQ35w9SQ4kx4
+7hZT+XwPDi9NQkO4x+fYK/CCLtopHSs5Y3u4Owyq2W6fDVy6qSrIaoZDjBZRQFe/
+WalmpDt3OiYhev6SAr4j65ehAxWL3fWCSRGrmbNjt+RLSYviFEjgBMzfoUJBmTCJ
+bifXIrkYY5bNRPoG7UE6EmUGhnxS4kORP60GhTPe281YLrijq/4bTV9EIZEKC3pH
+jshKctjaJOPmGSdv4q/cb/0sPqIxPGbo7bXgCdtJXvxKWj19GC6oplLI6RlDLngh
+gQIDAQAB';
 
-use role accountadmin;
+
+DESC USER snemer
+  ->> SELECT SUBSTR(
+        (SELECT "value" FROM $1
+           WHERE "property" = 'RSA_PUBLIC_KEY_FP'),
+        LEN('SHA256:') + 1) AS key;
+--use role accountadmin;
 
 -- should return 0 (due to deploy_pipeline_dev.yml)
-show parameters like 'data_retention_time_in_days' in table quickstart_dev.gold.vacation_spots;
+--show parameters like 'data_retention_time_in_days' in table quickstart_dev.gold.vacation_spots;
 -- should return 1 (due to deploy_pipeline_dev.yml)
-show parameters like 'data_retention_time_in_days' in table quickstart_prod.gold.vacation_spots;
+--show parameters like 'data_retention_time_in_days' in table quickstart_prod.gold.vacation_spots;
